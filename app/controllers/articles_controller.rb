d12a6f63b37f75871460a584168c640b129e4436
Rahul Patel <patelrahul19395@gmail.com>
2019-05-26 05:15:49 -0700

    messages add done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ end
 
   if @article.save
 
-flash[:notice] = "Article was successfully created"
+flash[:success] = "Article was successfully created"
 
   redirect_to article_path(@article)
 else
@@ -31,7 +31,7 @@ def update
 
 if @article.update(article_params)
 
-flash[:notice] = "Article was successfully updated"
+flash[:success] = "Article was successfully updated"
 
 redirect_to article_path(@article)
 
@@ -50,7 +50,7 @@ def destroy
 
 @article.destroy
 
-flash[:notice] = "Article was successfully deleted"
+flash[:danger] = "Article was successfully deleted"
 
 redirect_to articles_path
 
