e2596f3ce08a8302ef906173bbe2dc2b0d166322
Rahul Patel <patelrahul19395@gmail.com>
2019-05-30 01:57:04 -0700

    some issue fix abd add image

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,44 +3,34 @@ class ArticlesController < ApplicationController
 before_action :set_article, only: [:edit, :update, :show, :destroy]
 before_action :require_user, except: [:index, :show]
 before_action :require_same_user, only: [:edit, :update, :destroy]
-def index
+ def index
     @articles =  Article.all
-end
-
+ end
   def new
     @article = Article.new
-end
-def edit
+  end
+  def edit
 
-end
+   end
   def create
-  @article = Article.new(article_params)
-  @article.user = current_user
-  if @article.save
+     @article = Article.new(article_params)
+     @article.user = current_user
+       if @article.save
+         flash[:success] = "Article was successfully created"
+         redirect_to article_path(@article)
+       else
+         render 'new'
+      end
 
-flash[:success] = "Article was successfully created"
-
-  redirect_to article_path(@article)
-else
-render 'new'
-end
 end
 def update
-
-@article = Article.find(params[:id])
-
-if @article.update(article_params)
-
-flash[:success] = "Article was successfully updated"
-
-redirect_to article_path(@article)
-
-else
-
-render 'edit'
-
+  @article = Article.find(params[:id])
+  if @article.update(article_params)
+    flash[:success] = "Article was successfully updated"
+    redirect_to article_path(@article)
+  else
+   render 'edit'
 end
-
 end
 def show
 
@@ -55,6 +45,7 @@ flash[:danger] = "Article was successfully deleted"
 redirect_to articles_path
 
 end
+
   private
   def set_article
 
@@ -72,4 +63,5 @@ def require_same_user
         flash[:danger]="You can only edit or delete your own articles"
         redirect_to root_path
       end
+    end
 end
