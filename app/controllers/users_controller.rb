133a9253d502c7d8da2c0945147889f8f6398154
Rahul Patel <patelrahul19395@gmail.com>
2019-05-30 02:52:15 -0700

    ass sign in upon sogn up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the alpha blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
        render 'new'
      end
@@ -48,7 +49,7 @@ end
     end
 
     def require_same_user
-    if current_user != @user 
+    if current_user != @user
       flash[:danger] = "You can edit your own account"
       redirect_to root_path
     end
